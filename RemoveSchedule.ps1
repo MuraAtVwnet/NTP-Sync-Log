56c16cd9863650c723584e007b02a7ea92711039
Syuichi Murashima <mura+develop@vwnet.jp>
2018-07-16 13:33:54 +0900

    管理権限チェック入れた

--- RemoveSchedule.ps1
+++ RemoveSchedule.ps1
@@ -1,3 +1,13 @@
 ﻿# スケジュール削除
+
+# 管理権限で実行されていなかったらスクリプトを終了する
+if (-not(([Security.Principal.WindowsPrincipal] `
+	[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+	[Security.Principal.WindowsBuiltInRole] "Administrator"`
+	))) {
+	echo "[FAIL] You must have administrative rights."
+	exit
+}
+
 SCHTASKS /Delete /tn "MURA\NTP Log" /F
 SCHTASKS /Delete /tn "MURA\Remove NTP Log" /F
